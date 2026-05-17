20f1da6b865c4306e414f6ade376c297cb80449f
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-05-17 14:56:58 +0530

    Add solution: Two Sum II - Input Array Is Sorted

---
+++ Solutions/Array/2-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,25 @@
+// Welcome to WorkPad
+// Start coding here...
+
+public class TwoSumSortedTwoPointerApproach {
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[] {1,2,3,4,5,6},9));
+       
+    }
+
+    public static int[] twoSum(int[] nums,int target) {
+        int left=0,right=nums.length-1;
+        while(left < right) {
+            int sum = nums[left]+nums[right];
+            if(sum == target) {
+                return new int[] {left+1,right+1};
+            }
+            if(sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return null;
+    }
+}
