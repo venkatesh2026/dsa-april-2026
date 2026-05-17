a4868b79a52a1b42101e4431f72bf0014c991d83
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-05-17 15:53:48 +0530

    Add solution: Remove Duplicates from Sorted Array

--- Solutions/Array/3-remove-duplicates-from-sorted-array.java
+++ Solutions/Array/3-remove-duplicates-from-sorted-array.java
@@ -2,23 +2,22 @@
 // Start coding here...
 import java.util.Arrays;
 
-public class  3-remove-duplicates-from-sorted-array{
+public class 3-remove-duplicates-from-sorted-array{
     public static void main(String[] args) {
-        int[] nums = new int[] {1,1,2,2,3,4,4,5,6,6,7};
+        int[] nums = new int[] {1,1,2,2,3,4,4,5,6,6,7,9};
         System.out.println(removeDuplicates(nums));
         System.out.println(Arrays.toString(nums));
     }
 
     public static int removeDuplicates(int[] nums) {
-        int i = 0;
-        int j = 1;
-        while (j < nums.length) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+       int slow = 0, fast = 1;
+        while (fast < nums.length) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
             }
-            j++;
+            fast++;
         }
-        return i + 1;
+        return slow + 1;
     }
 }
