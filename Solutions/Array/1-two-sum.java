155dfeb550b75c4f9c290d694cb8675b1f85f565
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-06-09 19:50:39 +0530

    Add solution: Two Sum

--- Solutions/Array/1-two-sum.java
+++ Solutions/Array/1-two-sum.java
@@ -1,18 +1,8 @@
-import java.util.*;
-
-public class 1-two-sum{
-    public static void main(String[] args) {
-       System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15},9)));
-    }
-
-     public static  int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map= new HashMap();
-        for(int i=0;i<nums.length;i++) {
-            if(map.containsKey(target-nums[i])) {
-                return new int[] {map.get(target-nums[i]),i};
+ HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
-        return null;
-     }
-}
+        return new int[]{};
