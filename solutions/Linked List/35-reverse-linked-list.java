9fb7dce8c37e9ee9d8db5b7060ed6409f9a16172
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-06-30 14:14:44 +0530

    Add solution: Reverse Linked List

---
+++ solutions/Linked List/35-reverse-linked-list.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+
+        return previous;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
