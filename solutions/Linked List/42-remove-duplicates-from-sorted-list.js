2a67d7aaedf2146d1e7eae9d4edeadd6ebbb4022
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-07-09 23:13:10 +0530

    Add solution: Remove Duplicates from Sorted List

---
+++ solutions/Linked List/42-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,39 @@
+package LinkedList;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
