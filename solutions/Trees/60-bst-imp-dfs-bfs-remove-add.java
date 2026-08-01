0cfa53367d35d4e6a8be681a628db09830ddfa78
venkatesh2026 <dabbaravenkatesh2026@gmail.com>
2026-08-01 23:30:36 +0530

    Add solution: BST IMP(DFS,BFS, Remove, Add)

---
+++ solutions/Trees/60-bst-imp-dfs-bfs-remove-add.java
@@ -0,0 +1,81 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinarySearchTreeImplementation {
+    private Node root;
+
+    public BinarySearchTreeImplementation() {
+    }
+
+    public void add(int val) {
+        Node node = new Node(val);
+        if (root == null) {
+            root = node;
+            return;
+        }
+        Node current = root;
+        while (current != null) {
+            if (val >= current.val) {
+                if (current.right != null) {
+                    current = current.right;
+                } else {
+                    current.right = node;
+                    return;
+                }
+            } else {
+                if (current.left != null) {
+                    current = current.left;
+                } else {
+                    current.left = node;
+                    return;
+                }
+            }
+        }
+    }
+
+    public boolean search(int val) {
+        Node current = root;
+
+        while (current != null) {
+            if (current.val == val) {
+                return true;
+            }
+            if (val > current.val) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return false;
+    }
+
+
+
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTreeImplementation bstImpl = new BinarySearchTreeImplementation();
+        bstImpl.add(10);
+        bstImpl.add(11);
+        bstImpl.add(8);
+        bstImpl.add(9);
+        bstImpl.add(7);
+        bstImpl.add(6);
+        Node root = bstImpl.root;
+        System.out.println(root);
+        System.out.println(bstImpl.search(9));
+        System.out.println(bstImpl.search(12));
+    }
+}
